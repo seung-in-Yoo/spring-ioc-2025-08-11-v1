3c8b350b88d7abe930cb252fda7a53b7e85563a4
AEU <aeuplayground@gmail.com>
2025-08-13 16:56:46 +0900

    [Feat] ApplicationContextTest Green 완료

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,33 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
+import com.ll.domain.testPost.testPost.service.TestPostService;
+
 public class ApplicationContext {
+    TestPostRepository testPostRepository;
+    TestPostService testPostService;
+    TestFacadePostService testFacadePostService;
+
     public ApplicationContext() {
 
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        if (testFacadePostService == null) {
+            if (testPostService==null) {
+                if (testPostRepository==null) testPostRepository = new TestPostRepository();
+                testPostService = new TestPostService(testPostRepository);
+            }
+            testFacadePostService = new TestFacadePostService(testPostService, testPostRepository);
+        }
+
+
+        return (T) switch (beanName) {
+            case "testPostService" -> testPostService;
+            case "testPostRepository" -> testPostRepository;
+            case "testFacadePostService" -> testFacadePostService;
+            default -> throw new IllegalStateException("Unexpected value: " + beanName);
+        };
     }
 }
